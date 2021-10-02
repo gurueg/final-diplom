612fae39862c38672a74e0cfd730461ab079fa58
Alexander Kozicyn <alexander.kozicyn@theportalsoftware.com>
2021-10-02 12:41:05 +0300

    Added DRF-throttling

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -180,6 +180,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/sec',
+        'user': '3/sec'
+    },
 }
 
 SPECTACULAR_SETTINGS = {
